7738563af680ab25c390a2f5635169c4441b4bde
LokerL <806076693@qq.com>
2022-07-14 20:58:39 +0800

    fixed avatar size bug

--- src/components/QunMenu.vue
+++ src/components/QunMenu.vue
@@ -5,8 +5,8 @@
         <el-badge :value="p.name" type="primary">
           <el-avatar
             :shape="config.shape"
-            :size="config.size"
             :src="p.avatar"
+            :size="size"
             :class="{ active: currentTab === index }"
             @click="avatarClick(index)"
           />
@@ -20,9 +20,19 @@
 import { ref } from "vue";
 import { config } from "../config/qunMenuConfig";
 let currentTab = ref(0);
+let width = document.body.clientWidth;
 function avatarClick(index) {
   console.log(index);
   currentTab.value = index;
+}
+
+let size = width / 18;
+
+if (width < 1000) {
+  size = width / (Math.ceil(width / 100) + 4);
+  if (size < 50) {
+    size = 50;
+  }
 }
 </script>
 
